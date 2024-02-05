d26e27a7b04c06065ce6a30220dd09900f7ed019
fernando <fernandoadt1@gmail.com>
2024-02-05 11:52:37 -0300

    introduced test containers utility class

---
+++ customer-service/src/tests/test-container-mongo.js
@@ -0,0 +1,30 @@
+const { GenericContainer } = require('testcontainers');
+
+const createMongoContainer = async () => {
+  const env = {
+    container: null,
+    mongoUrl: null,
+  };
+  try {
+    env.container = await new GenericContainer('mvertes/alpine-mongo:latest')
+      .withExposedPorts(27017)
+      .start();
+    env.mongoUrl = `mongodb://localhost:${env.container.getMappedPort(27017)}`;
+  } catch (err) {
+    console.error(`Error while setting up:. ${JSON.stringify(err)}`);
+    throw new Error(err);
+  }
+  return env;
+}
+
+const closeConnection = async (container) => {
+  setTimeout(async () => {
+    await container.stop();
+  }, 5000);
+  process.env.DB_URL = null;
+};
+
+module.exports = {
+  createMongoContainer,
+  closeConnection
+}
